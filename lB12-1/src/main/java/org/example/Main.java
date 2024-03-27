aa16165e5ca1ab06e8ccd85157f3e84a8153476b
HelderShow <62794362+HelderShow@users.noreply.github.com>
2024-03-27 09:51:58 +0300

    first commit

---
+++ lB12-1/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "ИМЯ";
+        System.out.println("Hello, " + name + "!");
+    }
+}
