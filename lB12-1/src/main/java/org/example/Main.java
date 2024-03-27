99fcf211dd53c86184b2e17cdfa35180f0446341
HelderShow <62794362+HelderShow@users.noreply.github.com>
2024-03-27 10:29:26 +0300

    final commit

--- lB12-1/src/main/java/org/example/Main.java
+++ lB12-1/src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         String name = "Имя";
-        System.out.println("Привет!, " + name + "!");
-        System.out.println("Как твои дела?");
+        System.out.println("Здраствуйте Анастасия Кибисова, " + name + "!");
+        System.out.println("Круто! Спасибо за службу");
     }
 }
