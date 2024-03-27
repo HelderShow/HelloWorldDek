a55bdf6639c3f6c58af5fcad9f271c7ab7d35888
HelderShow <62794362+HelderShow@users.noreply.github.com>
2024-03-27 12:32:00 +0300

    test c

--- lB12-1/src/main/java/org/example/Main.java
+++ lB12-1/src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         String name = "Имя";
-        System.out.println("Здраствуйте Анастасия Кибисова, " + name + "!");
+        System.out.println("Здраствуйте Dekanchuk, " + name + "!");
         System.out.println("Круто! Спасибо за службу");
     }
 }
