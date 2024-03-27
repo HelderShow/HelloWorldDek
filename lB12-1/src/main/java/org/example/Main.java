a9cab10f394b4c80816069dc08965478062ebdeb
HelderShow <62794362+HelderShow@users.noreply.github.com>
2024-03-27 12:10:05 +0300

    test commit for lab

--- lB12-1/src/main/java/org/example/Main.java
+++ lB12-1/src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         String name = "NAME";
         System.out.println("Hello World, " + name + "!");
-        System.out.println("Kak Dela?");
+        System.out.println("Postavte 10 please))))");
     }
 }
