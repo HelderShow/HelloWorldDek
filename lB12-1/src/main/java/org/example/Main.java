1639ad48e3528e70cf6219c50bc9e60b745f05dc
HelderShow <62794362+HelderShow@users.noreply.github.com>
2024-03-27 10:09:00 +0300

    222commit222

--- lB12-1/src/main/java/org/example/Main.java
+++ lB12-1/src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         String name = "NAME";
-        System.out.println("COOL!, " + name + "!");
-        System.out.println(" ");
+        System.out.println("Hello World, " + name + "!");
+        System.out.println("Kak Dela?");
     }
 }
