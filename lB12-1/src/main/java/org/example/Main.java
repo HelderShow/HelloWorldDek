7885c2cce330a887f40668b98503c2cfbd66c122
HelderShow <62794362+HelderShow@users.noreply.github.com>
2024-03-27 09:57:15 +0300

    second main commit

--- lB12-1/src/main/java/org/example/Main.java
+++ lB12-1/src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         String name = "ИМЯ";
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Круто 2, " + name + "!");
     }
 }
